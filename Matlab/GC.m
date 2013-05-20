9a0973928c54474631a586b4087c37236cea54a0
yohann Salaun <ysal1@free.fr>
2013-05-20 19:40:37 +0200

    finalized graph cut part

--- Matlab/GC.m
+++ Matlab/GC.m
@@ -5,26 +5,38 @@ generated_input = true;
 
 if generated_input
 	% size of the 3D picture
-	n = 50;
-	s = [n n+1 n+5];
+	n = 40;
+	s = [n n n];
 	% center of the ellipsoid
 	c = [n/2 n/2 n/2];
 	% axis directions
-	X1 = [2 3 0];
-	X2 = [0 2 6];
-	X3 = [0 0 4];
+	X1 = [1 0 0];
+	X2 = [0 0 1];
+	X3 = [0 1 0];
 	X = [X1/sqrt(sum(X1.*X1)) ; X2/sqrt(sum(X2.*X2)) ; X3/sqrt(sum(X3.*X3))];
 	% axis rays
-	R = [7 8 9 ];
+	R = [5 8 15];
 	% law inside and outside
-	m = [0.65 0.2];
-	sigma = [0.35 0.2];
+	m = [0.7 0.3];
+	sigma = [0.3 0.3];
 
 	% generate ellipsoid
 	ellipsoid = generate_ellipsoid(s, c, X, R, m, sigma);
-	% display a slice of the ellipsoid
-	surf(ellipsoid(:,:,20));
+    
+    m_color = [45 123];
+    sigma = [45 60];
+    
+    % generate ellipsoid
+	I = generate_ellipsoid(s, c, X, R, m, sigma);
 end
+
+% display ellipsoid
+M = Is_in (ellipsoid);
+hold on;
+scatter3(M(:,1),M(:,2),M(:,3));
+hold off;
+
+
 
 %% GRAPH CUT
 
@@ -32,4 +44,14 @@ end
 mex GC/GC.cpp
 
 % execute mex file
-[label_map] = GC(ellipsoid, ellipsoid, 0, 0);
+[label_map] = GC(I, ellipsoid, [c' X' R'], 0);
+
+l0 = label(label_map, 0);
+hold on;
+scatter3(l0(:,1),l0(:,2),l0(:,3));
+hold off;
+
+l0 = diffMaps(label_map, 0, ellipsoid, 0.5);
+hold on;
+scatter3(l0(:,1),l0(:,2),l0(:,3));
+hold off;
