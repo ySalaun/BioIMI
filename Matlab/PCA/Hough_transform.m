4633d0f10955294f1305c745785941f31e97583f
SvRaph <siverar@eleves.enpc.fr>
2013-05-31 17:20:56 +0200

    Correction NaN Hough Transform

--- Matlab/PCA/Hough_transform.m
+++ Matlab/PCA/Hough_transform.m
@@ -1,8 +1,8 @@
 function [ c, R, Vec] = Hough_transform( I, c_pca, R_pca, Vec_pca )
 % Application of the hough transform to get the precise elements of the
-% ellipsoïde : size of axis R and directions Vec
+% ellipsoï¿½de : size of axis R and directions Vec
 % I is the image of probabilities to be in the nodule
-% c_pca is the center of the ellipsoïde determined by the pca
+% c_pca is the center of the ellipsoï¿½de determined by the pca
 % R_pca is the R^3 vector of the size of the axis 
 % Vec_pca is the matrix 3x3 which lines are the vector of the axis 
 
@@ -39,7 +39,7 @@ for i=Int1
             for l=1:3
                 d = d + (sum( (voxel-c_pca).*Vec_pca(l,:) )/R_pca(l) )^2;
             end
-            if (d<=1) % si le voxel est dans l'ellipse testée
+            if (d<=1) % si le voxel est dans l'ellipse testï¿½e
                 efficiency=efficiency+I(i,j,k)-1/2;
             end
         end
@@ -63,7 +63,7 @@ for v1=1:size(x0_var,2)
                               
     c=c_pca+[x0_var(v1) y0_var(v2) z0_var(v3)];
     
-    % faire varier les vecteurs tout en gardant l'orthogonalité des axes
+    % faire varier les vecteurs tout en gardant l'orthogonalitï¿½ des axes
     
     X(1,:)=Vec_pca(1,:)*R_pca(1)+[x1_var(v4) y1_var(v5) z1_var(v6)];
      
@@ -87,8 +87,8 @@ for v1=1:size(x0_var,2)
         efficiency=-Inf;
     else
         X(3,:)=cross(X(1,:),X(2,:)); % produit vectoriel
-        X(3,:)=[X(3,1)/sqrt(sum(X(3,1).*X(3,1)))  X(3,2)/sqrt(sum(X(3,2).*X(3,2)))  X(3,3)/sqrt(sum(X(3,3).*X(3,3)))];
-        
+        X3n=X(3,:)*X(3,:)';
+        X(3,:)=X(3,:)/ X3n;
         X(3,:)=X(3,:)*(R(3)+r3_var(v9));
         if (sum(X(3,:)==0)==3)
           efficiency=-Inf; % supprimer le cas X3 vecteur nul
@@ -98,7 +98,7 @@ for v1=1:size(x0_var,2)
 %     A=[[X(2,2) X(2,3)];[X(1,2) X(1,3)]]
 %     b=[-X(3,1)*X(2,1);-X(3,1)*X(1,1)]
 %     sol =A\b
-%     if ( sum(sol<=Inf )==2 && efficiency~=-Inf ) %supprimer les cas NaN et double Inf de non résolution
+%     if ( sum(sol<=Inf )==2 && efficiency~=-Inf ) %supprimer les cas NaN et double Inf de non rï¿½solution
 %         compte=compte+1;
 %         if (sol(1)<Inf)
 %             X(3,2)=sol(1);
@@ -147,15 +147,15 @@ for v1=1:size(x0_var,2)
 %         X(3,3)=-X(3,1)*X(2,1)/X(2,3);
 %     end
 
-    %test1 = sum(X(1,:).*X(2,:)) % vérifer l'orthogonalité
+    %test1 = sum(X(1,:).*X(2,:)) % vï¿½rifer l'orthogonalitï¿½
     %test2 = sum(X(1,:).*X(3,:))
     %test3 = sum(X(3,:).*X(2,:))
     
     if (efficiency~=-Inf)
     
-    R(1)=sqrt( sum( X(1,:).*X(1,:) ) )
-    R(2)=sqrt( sum( X(2,:).*X(2,:) ) )
-    R(3)=sqrt( sum( X(3,:).*X(3,:) ) )
+    R(1)=sqrt( sum( X(1,:).*X(1,:) ) );
+    R(2)=sqrt( sum( X(2,:).*X(2,:) ) );
+    R(3)=sqrt( sum( X(3,:).*X(3,:) ) );
     
     X(1,:)=X(1,:)/R(1);
     X(2,:)=X(2,:)/R(2);
@@ -176,7 +176,7 @@ for i=Int1
                     testD3=voxel                   
                 end
             end
-            if (d<=1) % si le voxel est dans l'ellipse testée
+            if (d<=1) % si le voxel est dans l'ellipse testï¿½e
                 efficiency=efficiency+I(i,j,k)-1/2;
             end
         end
