2d8b1f273e40c1e6dfd10a2fd7c115cc6bb73d33
SvRaph <siverar@eleves.enpc.fr>
2013-06-04 01:30:41 +0200

    un 1 qui trainait

--- Matlab/Interface/classification_acp.m
+++ Matlab/Interface/classification_acp.m
@@ -21,14 +21,14 @@ Y=(Y-minY)/(maxY-minY);
 YY=zeros(siz(1),siz(2));
 for xx=1:siz(1)
     for yy=1:siz(2)
-        vec=squeeze(Y1(xx,yy,:));
+        vec=squeeze(Y(xx,yy,:));
         coeff=polyfit(1:siz(3),vec',2);
         a=coeff(1);
         b=coeff(2);
         L=siz(3);
         if (a<0) && (b> (-2*a)) && (b< (-2*a*siz(3))) && (-1*a*L*L > 0.01) &&  (-1*a*L > 1*abs(b))          % maximum sur la plage
             YY(xx,yy)=1;
-            disp(a);
+            %disp(a);
         end
     end
 end
