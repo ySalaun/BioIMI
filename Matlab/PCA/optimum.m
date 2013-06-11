7eeffcffa95d2d325e200e05e7976b5325e2fb61
mrozand <m.rozand@gmail.com>
2013-06-12 00:06:46 +0200

    optimisation de hough transform
    J'en suis à 3h15 pour ces variations de paramètres mais je sèche pour
    encore optimiser

---
+++ Matlab/PCA/optimum.m
@@ -0,0 +1,13 @@
+function [efficiency_max coeff_opt]= optimum (M,IResized,eff_max,coef_opt,c,R,X)
+
+    volume=sum(sum (sum(M)));
+
+    efficiency=sum(sum(sum(M.*IResized)))-1./2*volume;
+    
+    if ( efficiency>eff_max )
+        efficiency_max=efficiency;
+        coeff_opt = [ c R X(1,:) X(2,:) X(3,:) ]; 
+    else 
+        efficiency_max=eff_max;
+        coeff_opt=coef_opt;
+    end
